26b977ec96533f76f35c4f49611ff10ae76629f5
bigboss424 <ajones061392@gmail.com>
2015-04-01 20:37:55 -0700

    Added files for a Raspberry Pi Presentation that I am currently working on for the Linuxfest 2015. I believe the main topic and theme should be Transferring One's Workload to Linux. We shall see how the progress on this continues and I will be uploading all the files for this presentation as well. Feel free to dive in and give me some critiques if you will.

---
+++ Twisted/SecuritySystem/Java/objects/Customer.java
@@ -0,0 +1,10 @@
+/* Created by: Aaron Jones
+*  Date: 4/1/2015
+*  Description: This class is simply to be created for the purpose of creatin*g a new object called Customer. I will be working on creating the details fo*r the following class as I go along. There will be comments.
+*/
+
+public class Customer
+{
+	private 
+
+}//end class 
