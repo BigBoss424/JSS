a1ba7a2f2bc9436dded04cd56ca66d7a81103cdf
bigboss424 <ajones061392@gmail.com>
2015-04-03 09:19:11 -0700

    The following code has been added into Eclipse so there will be quite a bit of files that are related to the Eclipse IDE.

---
+++ Twisted/SecuritySystem/Java/utils/MenuSystem.java
@@ -0,0 +1,27 @@
+/**
+ * Created by: Aaron Jones
+ * Date: 4/3/2015
+ * Last Modified:
+ * Description: This class will basically hold all the menu driven options in the following
+ * menu system below in seperate methods. I plan on implementing this into the driver method
+ * for further testing. 
+ * 
+ * 
+ * Bugs:
+ *
+ *
+ * 
+ * Fixes: 
+ * 
+ * 
+ * 
+ * 
+ */
+
+/**
+ * @author bigboss424
+ *
+ */
+public class MenuSystem {
+
+}
