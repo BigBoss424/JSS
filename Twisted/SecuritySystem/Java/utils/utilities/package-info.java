33b4d4fb0f05e30f6c37dfda4239c7b13d98b32c
bigboss424 <ajones061392@gmail.com>
2015-04-03 09:22:04 -0700

    Created a package for the utilities and going to make a package for the main part of the program as well once I get there.

---
+++ Twisted/SecuritySystem/Java/utils/utilities/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * Created by: Aaron Jones
+ * Date: 
+ * Last Modified:
+ * Description: 
+ * 
+ * 
+ * Bugs:
+ *
+ *
+ * 
+ * Fixes: 
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @author bigboss424
+ *
+ */
+package utilities;
