e915c470ee5be3ba9a57369bccd8e63b2234d6bf
Aaron Jones <ajones061392@gmail.com>
2015-04-05 17:50:32 -0700

    Created the FileUtil class
    Created the FileUtil class for check in program. I believe adding the methods necessary to open up CSV files and other common database types is going to be vital in the creation of this program

---
+++ Twisted/SecuritySystem/Java/utils/utilities/FileUtil.java
@@ -0,0 +1,23 @@
+/*
+*   Created by: Aaron Jones
+*   Date: 4/5/15
+*   Description: This program will be used as a utility program to build the necessary functions for 
+*   extracting and inputing different data types for the different file types. The main support I want to 
+*   create first will be simply a text file. From there I plan on moving onto a .csv (Comma Seperated Values)
+*   to allow files to be written, read , manipulated by the program itself. The end goal being common support for all
+*   database values. 
+*
+*   Bugs: 
+*
+*
+*   Fixes: 
+* 
+*
+*
+*/
+
+public class FileUtil
+{
+
+
+}//end class
